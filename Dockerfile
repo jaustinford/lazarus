6e917678ee85ed5e7a09bb6fde37cbc200d6cd3a
james austin <j.austin.ford@gmail.com>
2025-04-16 00:01:31 -0600

    add apcups configs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 
-FROM --platform=arm64 docker:28.0.4-dind-alpine3.21
+FROM --platform=linux/arm64 docker:28.0.4-dind-alpine3.21
 
 RUN \
     apk add \
@@ -12,3 +12,6 @@ WORKDIR /lazarus
 
 COPY --chmod=755 \
     src/ ./src/
+
+COPY conf/ups-0.conf /etc/apcupsd/ups-0.conf
+COPY conf/ups-1.conf /etc/apcupsd/ups-1.conf
--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,7 @@ when to execute other docker container
 services on the system host.
 """
 
+import os
 import time
 
 def main():
@@ -11,6 +12,9 @@ def main():
     Execute conditionals and trigger
     automations.
     """
+
+    os.system("/sbin/apcupsd -f /etc/apcupsd/ups-0.conf")
+    os.system("/sbin/apcupsd -f /etc/apcupsd/ups-1.conf")
 
     while True:
         time.sleep(10)
