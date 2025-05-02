b9a3105825c30c82b68076d2985aa0f00975f433
james austin <j.austin.ford@gmail.com>
2025-05-01 22:41:08 -0600

    fix error handling and logging for elasticsearch

--- src/__main__.py
+++ src/__main__.py
@@ -26,28 +26,28 @@ def main():
     increment_counter = 0
 
     while True:
-        if not os.path.isfile(constants.CYCLES_LOCK_PATH) and increment_counter == 30:
-            es_client = elastic.connect_elasticsearch()
-            logs.GENERAL_LOGGER.info("Connected to Elasticsearch : %s", es_client.info())
+        if increment_counter == 30:
+            if not os.path.isfile(constants.CYCLES_LOCK_PATH):
+                es_client = elastic.connect_elasticsearch()
+                logs.GENERAL_LOGGER.info("Connected to Elasticsearch : %s", es_client.info())
 
-            if not es_client.indices.exists(index="apcups"):
-                elastic.create_index(es_client, "apcups")
+                if not es_client.indices.exists(index="apcups"):
+                    elastic.create_index(es_client, "apcups")
 
-            for combined_metric in apc.combine_metrics(constants.CONF_DIR):
-                logs.GENERAL_LOGGER.info("UPS metrics ingested : %s", str(combined_metric))
-                es_client.index(
-                    index="apcups",
-                    document=combined_metric
-                )
+                for combined_metric in apc.combine_metrics(constants.CONF_DIR):
+                    logs.GENERAL_LOGGER.info("UPS metrics ingested : %s", str(combined_metric))
+                    es_client.index(
+                        index="apcups",
+                        document=combined_metric
+                    )
+
+                es_client.close()
 
             increment_counter = 0
-
-            es_client.close()
 
         increment_counter += 1
 
         cycles.process_items()
-        logs.GENERAL_LOGGER.info("test iteration")
         time.sleep(1)
 
 if __name__ == "__main__":
