97c66484c3efd9c7bc322d65f3a57fa98c1636b3
Austin Ford <j.austin.ford@gmail.com>
2025-06-03 16:28:00 -0600

    bug fix with down cycle trigger

--- src/execute.py
+++ src/execute.py
@@ -100,11 +100,13 @@ def process_power(combined_metrics: list, power_counter: tuple):
     trigger_counter = (power_counter[0], power_counter[1])
     clear_counter   = (power_counter[2], power_counter[3])
 
+    power_lock = os.path.join(constants.DATA_DIR, "power.lock")
+
     determined_trigger = power.determine_event("ONBATT", combined_metrics, trigger_counter)
     determined_clear   = power.determine_event("ONLINE", combined_metrics, clear_counter)
 
     if determined_trigger[0]:
-        if not os.path.join(constants.DATA_DIR, "power.lock"):
+        if not os.path.isfile(power_lock):
             power.add_event("trigger", "down")
 
     else:
@@ -112,10 +114,8 @@ def process_power(combined_metrics: list, power_counter: tuple):
             power.add_event("clear", "up")
 
     return (
-        determined_trigger[1],
-        determined_trigger[2],
-        determined_clear[1],
-        determined_clear[2]
+        determined_trigger[1], determined_trigger[2],
+        determined_clear[1], determined_clear[2]
     )
 
 def process_ingest():
